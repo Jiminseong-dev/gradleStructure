ccca1287bfe3888b120e89c9957beb81a1ce4855
손민성 <son344910@onsecurity.co.kr>
2024-05-02 14:38:19 +0900

    add project

---
+++ src/main/groovy/com/defaultspring/defaultspringboot/DefaultSpringBootApplication.groovy
@@ -0,0 +1,13 @@
+package com.defaultspring.defaultspringboot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DefaultSpringBootApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DefaultSpringBootApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/defaultspring/defaultspringboot/DefaultSpringBootApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.defaultspring.defaultspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DefaultSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
